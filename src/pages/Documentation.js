b4843fbc20de380d160f20774d48f9ff314b251d
junhee916 <junhee916@naver.com>
2020-05-15 22:49:50 +0900

    D

--- src/pages/Documentation.js
+++ src/pages/Documentation.js
@@ -18,7 +18,7 @@ const Documentation = function(props){
                 <div className="document_menu">
                 <div className="document_menu_text" onClick={function(){props.history.push("/history")}}>Manual</div>
                 <div className="document_menu_text" onClick={function(){props.history.push("/login")}}>Login</div>
-                <a href="https://kauth.kakao.com/oauth/authorize?client_id=e94722859a13a96095d4b975965b3a3f&redirect_uri=http://localhost:3000/kakaoLogin&response_type=code"><div className="document_menu_text">Kakao Login</div></a>
+                <a href="https://kauth.kakao.com/oauth/authorize?client_id=e94722859a13a96095d4b975965b3a3f&redirect_uri=http://growactions.co.kr/kakaoLogin&response_type=code"><div className="document_menu_text">Kakao Login</div></a>
                 <div className="document_menu_text" onClick={function(){props.history.push("/agree")}}>회원가입</div>
                 </div>}
           </div>
